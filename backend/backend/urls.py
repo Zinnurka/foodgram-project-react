b31b43403ef746687cb4dc70ca6d917db52275df
Zinnurov Ramil <ramil.zinnurov@infotecs.ru>
2022-08-15 22:26:23 +0500

    add authorization with token

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
     path('api/', include('api.urls')),
 ]
 
