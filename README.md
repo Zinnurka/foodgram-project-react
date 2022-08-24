be995d947a30a7fc915138b517f0bf7d44a320da
Zinnurov Ramil <ramil.zinnurov@infotecs.ru>
2022-08-24 13:17:00 +0500

    уже не используется

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ![example workflow](https://github.com/Zinnurka/foodgram-project-react/actions/workflows/foodgram_workflow.yml/badge.svg) 
 
 ## Проект в интернете
-Проект запущен и доступен по [адресу](http://62.84.126.201/recipes)
+Проект запущен и доступен по [адресу](http://84.201.132.246//recipes)
 
 
 ## Подготовка и запуск проекта
--- backend/recipe/validators.py
+++ backend/recipe/validators.py
@@ -3,14 +3,6 @@ import base64
 from django.core.exceptions import ValidationError
 
 
-def validate_cooking_time(value):
-    if value < 1:
-        raise ValidationError(
-            'Значение должно быть положительным',
-            params={'value': value},
-        )
-
-
 def validate_image(value):
     if not base64.b64decode(value):
         raise ValidationError(
